c6d607b857d9f1c5d820885dc9c997bce253c91d
ApxNord <dronov_a@t.top-academy.ru>
2022-12-28 21:11:54 +0300

    Добавьте файлы проекта.

---
+++ Practic28.12/Practic28.12.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/*
+Написать ф-ю, которая принимает координаты шахматной фигуры
+куда нужно сходить.
+Ф-я должна возвращать true или false, если этот ход возможен
+  0 1 2 3 4 5 6 7	- B
+0|*|_|_|_|*|_|_|_|
+1|_|*|_|_|*|_|_|*|
+2|_|_|*|_|*|_|*|_|
+3|_|_|_|*|*|*|_|_|
+4|*|*|*|*|P|*|*|*|
+5|_|_|_|*|*|*|_|_|
+6|_|_|*|_|*|_|*|_|
+7|_|*|_|_|*|_|_|*|
+
+|
+A
+*/
+//для пешки
+const int centerA = 4, centerB = 4;
+bool IsMovePawn(int a, int b) {
+	return (centerB == b && centerA == a - 1) ? true : false;
+}
+//для слона
+bool IsMoveElephant(int a, int b) {
+	return (abs(centerA - a) == abs(centerB - b)) ? true : false;
+}
+//для ладьи
+bool IsMoveRook(int a, int b) {
+	return (centerA == a || centerB == b)? true : false;
+}
+//ход коня
+bool IsMoveHorse(int a, int b) {
+	return 
+		(( abs(centerA - a) == 1 && abs (centerB - b) == 2) || 
+		 ( abs(centerA - a) == 2 && abs (centerB - b) == 1)) ? true : false;
+}
+//ход королевы
+bool IsMoveQueen(int a, int b) {
+	return (IsMoveElephant(a, b) || IsMoveRook(a, b))  ? true : false;
+}
+
+
+
+int main()
+{
+	/*
+	srand(time(0));
+
+	// генерация чисел от 5 до 23
+	int arr[5];
+	for (int i = 0; i < 5; i++) {
+		arr[i] = 5 + rand() % 18; // 5 - 23
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+
+	//генерация вещественных чисел от 2.0 до 5.0
+	double arrDouble[5];
+	for (int i = 0; i < 5; i++) {
+		arrDouble[i] = 2 + rand() % 3 + (double)(rand() % 100) / 100;
+		cout << arrDouble[i] << " ";
+	}
+	cout << endl;
+
+	//генерация целы чисел от -10 до 10
+	int arrInt[10];
+	for (int i = 0; i < 10; i++) {
+		arrInt[i] = rand() % 10 - rand() % 10;
+		cout << arrInt[i] << " ";
+	}
+	*/
+
+
+}
---
+++ TicTacToe28.12/TicTacToe28.12.cpp
@@ -0,0 +1,73 @@
+﻿// TicTacToe28.12.cpp
+
+#include <iostream>
+
+using namespace std;
+
+// Игра "Крестики - Нолики"
+
+#define S(X) (X) ? 'X' : 'O' // макрос для назначения символа
+
+const int SIZE_BOARD = 3; // размер доски
+char board[SIZE_BOARD][SIZE_BOARD]{ '_','_','_','_','_','_',' ',' ',' ' };
+
+
+void ShowBoard(); // отображение поля
+int PlayerTurn(); //ход игрока
+int ComputerTurn(); // ход компьютера
+void LogicGame(); // логика игры
+void InicialBoard(int a, char c); // занесение в массив хода игроков
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    LogicGame();
+}
+
+void ShowBoard() {
+    for (int i = 0; i < SIZE_BOARD; i++) {
+        for (int j = 0; j < SIZE_BOARD; j++) {
+            if (j != 0) cout << '|';
+            cout << board[i][j];
+        }
+        cout << endl;
+    }
+}
+
+int PlayerTurn() {
+    int pl;
+    while (true)
+    {
+        cout << "Введите X и Y (слитно, без пробела) -> "; cin >> pl;
+        if (pl <= 33 || pl >= 11) return pl;
+        else cout << "Введено неверное значение, повторите попытку\n";
+        system("cls");
+    }
+}
+int ComputerTurn() {
+    srand(time(0));
+    return rand()%3*10 + rand() % 3;
+}
+void InicialBoard(int a, char c) {
+    int x = a / 10;
+    int y = a % 10;
+    if (board[x][y] == 'X' || board[x][y] == 'O') {
+        cout << "Нельзя. Клетка уже задана\n";
+    }
+    else
+        board[x][y] = c;
+}
+void LogicGame() {
+    ShowBoard();
+
+    srand(time(0));
+    int step = rand() % 2;
+    char player = S(step), comp = S(!step);
+    while (true)
+    {
+        InicialBoard((step) ? PlayerTurn() : ComputerTurn(),
+                     (step)? player : comp);
+
+        ShowBoard();
+    }
+}
