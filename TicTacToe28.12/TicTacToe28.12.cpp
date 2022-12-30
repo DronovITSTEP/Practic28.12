43fc1b1b1cc1a21e79263aa57323ab84dbf28795
ApxNord <dronov_a@t.top-academy.ru>
2022-12-30 20:09:13 +0300

    Готовый вариант

--- TicTacToe28.12/TicTacToe28.12.cpp
+++ TicTacToe28.12/TicTacToe28.12.cpp
@@ -1,6 +1,7 @@
 ﻿// TicTacToe28.12.cpp
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,20 +12,26 @@ using namespace std;
 const int SIZE_BOARD = 3; // размер доски
 char board[SIZE_BOARD][SIZE_BOARD]{ '_','_','_','_','_','_',' ',' ',' ' };
 
+int compStep[9]{ 00, 01, 02, 10, 11, 12, 20, 21, 22 }; // возможные ходы 
+
 
 void ShowBoard(); // отображение поля
 int PlayerTurn(); //ход игрока
 int ComputerTurn(); // ход компьютера
 void LogicGame(); // логика игры
-void InicialBoard(int a, char c); // занесение в массив хода игроков
+bool InicialBoard(int a, char c); // занесение в массив хода игроков
+bool IsWin(char c); // проверка выйгрыша
 
 int main()
 {
     setlocale(LC_ALL, "");
+    
+    srand(time(0));   
+    random_shuffle(begin(compStep), end(compStep)); // перемешивет значения местами в массиве
+    
     LogicGame();
 }
 
-//все ок
 void ShowBoard() {
     for (int i = 0; i < SIZE_BOARD; i++) {
         for (int j = 0; j < SIZE_BOARD; j++) {
@@ -34,7 +41,6 @@ void ShowBoard() {
         cout << endl;
     }
 }
-
 int PlayerTurn() {
     int pl;
     while (true)
@@ -48,19 +54,35 @@ int PlayerTurn() {
         //system("cls");
     }
 }
-int ComputerTurn() {
-    srand(time(0));
-    return (rand()%3)*10 + rand() % 3;
+int ComputerTurn() {  
+    for (static int i = 0; i < 9;) {
+        return compStep[i++];
+    }
 }
-
-void InicialBoard(int a, char c) {
+bool InicialBoard(int a, char c) {
     int x = a / 10;
     int y = a % 10;
     if (board[x][y] == 'X' || board[x][y] == 'O') {
         cout << "Нельзя. Клетка уже задана\n";
+        return false;
     }
     else
         board[x][y] = c;
+    return true;
+}
+bool IsWin(char c) {
+    for (int i = 0; i < 3; i++) { // проверка по строкам
+        if (board[i][0] == c && board[i][1] == c && board[i][2] == c) return true;
+    }
+    for (int j = 0; j < 3; j++) { // проверка по столбцам
+        if (board[0][j] == c && board[1][j] == c && board[2][j] == c) return true;
+    }
+
+    // проверка по диагонали
+    if (board[0][0] == c && board[1][1] == c && board[2][2] == c) return true;
+    if (board[0][2] == c && board[1][1] == c && board[2][0] == c) return true;
+
+    return false;
 }
 
 void LogicGame() {
@@ -69,11 +91,20 @@ void LogicGame() {
     srand(time(0));
     int step = rand() % 2;
     char player = S(step), comp = S(!step);
-    while (true)
+
+    int count = 0;
+    while (count <= 9)
     {
-        InicialBoard((step) ? PlayerTurn() : ComputerTurn(),
-                     (step)? player : comp);
+        if (!InicialBoard((count % 2) ? PlayerTurn() : ComputerTurn(),
+            (count % 2) ? player : comp)) continue;
 
         ShowBoard();
+
+        if (IsWin((count % 2) ? player : comp)) {           
+            cout << "Победа!\n";           
+            break;
+        }
+        count++;       
     }
+    if (count > 9) cout << "Ничья!\n";
 }
